207a645b3712c68710556cf38e7fc24329987d6e
DominiqueNB <dominique.nascimento@gmail.com>
2020-11-23 19:30:01 -0400

    Fix constaints on Subscription, BankMenu and FinancialDetails screens

--- PI_G8_BeneficieApp/PI_G8_BeneficieApp/User_BanksMenu/BanksMenuViewController.swift
+++ PI_G8_BeneficieApp/PI_G8_BeneficieApp/User_BanksMenu/BanksMenuViewController.swift
@@ -46,6 +46,15 @@ extension BanksMenuViewController: UICollectionViewDelegate {
     }
 }
 
+extension BanksMenuViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 128, height: 128)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+}
+
 extension BanksMenuViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return arrayBanks.count
@@ -59,3 +68,4 @@ extension BanksMenuViewController: UICollectionViewDataSource {
     
     
 }
+
