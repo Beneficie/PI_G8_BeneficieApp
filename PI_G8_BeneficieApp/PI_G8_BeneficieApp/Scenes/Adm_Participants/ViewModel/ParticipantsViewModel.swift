8fdb049186e12e57baf2b3d1589c4d2483e51a53
DominiqueNB <dominique.nascimento@gmail.com>
2020-12-07 09:03:09 -0400

    Architecture setup

---
+++ PI_G8_BeneficieApp/PI_G8_BeneficieApp/Scenes/Adm_Participants/ViewModel/ParticipantsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ParticipantsViewModel.swift
+//  PI_G8_BeneficieApp
+//
+//  Created by Dominique Nascimento Bezerra on 07/12/20.
+//  Copyright © 2020 Juan Souza. All rights reserved.
+//
+
+import Foundation
+
+class ParticipantsViewModel {
+    
+}
---
+++ PI_G8_BeneficieApp/PI_G8_BeneficieApp/Scenes/SingUp/ViewModel/SignUpViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpViewModel.swift
+//  PI_G8_BeneficieApp
+//
+//  Created by Dominique Nascimento Bezerra on 07/12/20.
+//  Copyright © 2020 Juan Souza. All rights reserved.
+//
+
+import Foundation
+
+class SignUpViewModel {
+    
+}
