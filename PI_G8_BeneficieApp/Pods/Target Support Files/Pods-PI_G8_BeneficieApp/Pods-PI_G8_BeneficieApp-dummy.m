9fc34a4d0af5897f11e016a92540e18f3e240bf6
Juan Souza <juan.m.souza@gmail.com>
2020-12-21 22:21:23 -0300

    add new info for singup

---
+++ PI_G8_BeneficieApp/Pods/Target Support Files/Pods-PI_G8_BeneficieApp/Pods-PI_G8_BeneficieApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PI_G8_BeneficieApp : NSObject
+@end
+@implementation PodsDummy_Pods_PI_G8_BeneficieApp
+@end
---
+++ PI_G8_BeneficieApp/Pods/Target Support Files/Pods-PI_G8_BeneficieApp/Pods-PI_G8_BeneficieApp-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_PI_G8_BeneficieAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PI_G8_BeneficieAppVersionString[];
+
