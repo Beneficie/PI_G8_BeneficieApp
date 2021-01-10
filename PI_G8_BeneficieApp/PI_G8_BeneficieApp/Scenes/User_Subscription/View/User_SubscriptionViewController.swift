8716b954f0476f0c61996504be396f7f02f950e8
Dominique <dominique.nascimento@gmail.com>
2021-01-09 21:14:14 -0400

    Check if user is subscribed

--- PI_G8_BeneficieApp/PI_G8_BeneficieApp/Scenes/User_Subscription/View/User_SubscriptionViewController.swift
+++ PI_G8_BeneficieApp/PI_G8_BeneficieApp/Scenes/User_Subscription/View/User_SubscriptionViewController.swift
@@ -53,6 +53,7 @@ class User_SubscriptionViewController: UIViewController {
             if success {
                 self.event = self.viewModel.arrayEvents[0]
                 self.setUpUI(event: self.event)
+                self.availabilityToSubscribe()
             } else {
                 print("FailInLoadData")
                 self.alertFailedInLoadData()
@@ -76,15 +77,31 @@ class User_SubscriptionViewController: UIViewController {
         present(alert, animated: true)
     }
     
-    func availabilityToSubscribe(vacancy: Int) {
-        if vacancy > 0 {
-            buttonSubscribe.backgroundColor = UIColor(red: 115/255, green: 121/255, blue: 224/255, alpha: 1.0)
-            buttonSubscribe.isEnabled = true
-        } else {
+    func canUserSubscribe() -> Bool {
+        if event.subgrupos[0].inscritos.contains(currentUser.nome) {
             buttonSubscribe.backgroundColor = .lightGray
             buttonSubscribe.isEnabled = false
+            return false
+        } else {
+            return true
         }
     }
+    
+    func availabilityToSubscribe() {
+        if canUserSubscribe() {
+            let vacancy = event.subgrupos[0].vagasDisponiveisSubgrupo
+            if vacancy > 0 && event.subgrupos[0].inscritos.contains(currentUser.nome) {
+                buttonSubscribe.backgroundColor = UIColor(red: 115/255, green: 121/255, blue: 224/255, alpha: 1.0)
+                buttonSubscribe.isEnabled = true
+            } else {
+                buttonSubscribe.backgroundColor = .lightGray
+                buttonSubscribe.isEnabled = false
+            }
+        }
+    }
+    
+    
+    
     @IBAction func backButton(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
@@ -117,7 +134,7 @@ extension User_SubscriptionViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let vacancy = viewModel.arrayEvents[0].subgrupos[row].vagasDisponiveisSubgrupo
         labelEventSubGroupVacancies.text = String(vacancy)
-        self.availabilityToSubscribe(vacancy: vacancy)
+        self.availabilityToSubscribe()
     }
     
 }
